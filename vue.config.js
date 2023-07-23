393f9e94e13c4b0b85e9d7db1f2256f5c30776cf
alex <aglynn1667@gmail.com>
2023-07-23 10:27:37 -0400

    favicon

--- vue.config.js
+++ vue.config.js
@@ -6,7 +6,12 @@ module.exports = {
     msTileColor: '#000000',
     appleMobileWebAppCapable: 'yes',
     appleMobileWebAppStatusBarStyle: 'black',
-
+	iconPaths: {
+		faviconSVG: 'img/icons/favicon.svg',
+		favicon32: 'img/icons/favicon-32x32.png',
+		favicon16: 'img/icons/favicon-16x16.png',
+		appleTouchIcon: 'img/icons/apple-touch-icon.png'
+	},
     // configure the workbox plugin
     workboxPluginMode: 'InjectManifest',
     workboxOptions: {
