53a51b4262b7ef9c5ad0501d5e039616a5364367
Alex <alex.glynn@louisville.edu>
2024-04-04 20:38:40 -0400

    Test body background color

--- src/App.vue
+++ src/App.vue
@@ -173,13 +173,16 @@ const del = () => {
   font-family: 'Linux Biolinum', sans-serif;
 }
 
+body {
+  background-color: black;
+}
+
 main {
   width: 100%;
   height: 100%;
   display: flex;
   flex-direction: column;
   justify-content: space-between;
-  background-color: black;
 }
 
 #screen::-webkit-scrollbar {
